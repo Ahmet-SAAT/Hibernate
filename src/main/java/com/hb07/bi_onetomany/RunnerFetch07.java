ab674649f952f1bb03293d8542edcb4bb67026f5
Ahmet-SAAT <avahmetsaat@gmail.com>
2023-03-30 00:02:27 +0300

    30.03.2023

--- src/main/java/com/hb07/bi_onetomany/RunnerFetch07.java
+++ src/main/java/com/hb07/bi_onetomany/RunnerFetch07.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class RunnerFetch07 {
     public static void main(String[] args) {
 
@@ -12,19 +14,58 @@ public class RunnerFetch07 {
                 addAnnotatedClass(Student07.class).addAnnotatedClass(Book07.class);
 
         SessionFactory sf = con.buildSessionFactory();
-        Session session =  sf.openSession();
+        Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
 
-       Student07 student=session.get(Student07.class,1001);
+        Student07 student = session.get(Student07.class, 1001);
         System.out.println(student);
 
 // !!! SQL ile öğrenci bilgilerini ( Inner Join )  alalım ( öğrenci ismi ve kitap ismi )
 
 // !!! yukardaki sorguyu HQL ile yazalım
+
+        //delete islemi
+        // !!! Delete islemi SQL
+       /* String sqlQuery1 = "DELETE FROM book07";
+        int numOfDeletedRecords = session.createSQLQuery(sqlQuery1).executeUpdate();
+        System.out.println("Silinen kayit sayisi : " + numOfDeletedRecords);*/
+
+        /*String sqlQuery2 = "DELETE FROM student07";
+        int numOfDeletedRecords2 = session.createSQLQuery(sqlQuery2).executeUpdate();
+        System.out.println("Silinen kayit sayisi : " + numOfDeletedRecords2);*/
+     //hql ile delete islemi
+      /*  String hql1 = "Delete from Book07";
+        int updated1 = session.createSQLQuery(hql1).executeUpdate();
+        System.out.println(updated1);*/
+
+       /* String hql2 = "Delete from Student07";
+        int updated2 = session.createSQLQuery(hql2).executeUpdate();
+        System.out.println(updated2);*/
+//kitap ismi aA book olan kitabi hql ile sil
+     /*   String hql3="Delete from Book07 b where b.name='A Book'";
+        int updated3=session.createQuery(hql3).executeUpdate();
+        System.out.println(updated3);*/
+//kitap bilgisi olan ogrencileri sil
+        //1. yol once book tablosunda iliskili olanlar silinir sonra studenttakile
+        //2.yol student classina cascade ya da orphenremoval
+
+        //1001 idli student objesini silelim
+
+        Student07 student2= session.get(Student07.class,1001);
+       session.delete(student2);
+
+        student2.getBookList().set(0,null);
+
+        //// !!! book ismi içinde "Book" geçen student kayıtlarını alalım ( HQL )
+        String hql4="Select s from Student07 s join s.booklist b where b.name like '%Book%'";
+        List<Student07> resultlist4 =session.createQuery(hql4, Student07.class).getResultList();
+        for ( Student07 std:resultlist4){
+            System.out.println(std);
+        }
+
         tx.commit();
         session.close();
         sf.close();
-
 
 
     }
--- src/main/java/com/hb08/manytomany/Student08.java
+++ src/main/java/com/hb08/manytomany/Student08.java
@@ -1,52 +1,65 @@
-package com.hb07.bi_onetomany;
+package com.hb08.manytomany;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
-public class Student07 {
+public class Student08 {
     @Id
     private int id;
     @Column(name="student_name", nullable = false)
     private String name;
     private int grade;
-    @OneToMany(mappedBy = "student",cascade = CascadeType.REMOVE,orphanRemoval = true)
-    //ikisi beraber olmaz.Orpanda nula cektigimiz child bilgilerini de siler.
-    //CascadeType.Persist de ise parentte save yaparsan save kendinden olur.
-    private List<Book07> bookList = new ArrayList<>();
-    // !!! Getter-Setter
+
+    @ManyToMany
+     @JoinTable(//tablo olusturuldu
+             name="student08_book08",
+             joinColumns = {@JoinColumn(name="std_id")},//icinde bulundugum classin idsi ile sutun olustu
+             inverseJoinColumns = {@JoinColumn(name="book_id")}//diger classin idsi ile sutun olustu
+     )
+    //3. bir tablo olusturuyorum.Hem student hem book bilgisi tutacak
+    private List<Book08> booklist=new ArrayList<>();
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getGrade() {
         return grade;
     }
+
     public void setGrade(int grade) {
         this.grade = grade;
     }
-    public List<Book07> getBookList() {
-        return bookList;
+
+    public List<Book08> getBooklist() {
+        return booklist;
     }
-    //    public void setBookList(List<Book07> bookList) {
-//        this.bookList = bookList;
-//    }
-    // !!! toString()
+
+    public void setBooklist(List<Book08> booklist) {
+        this.booklist = booklist;
+    }
+
     @Override
     public String toString() {
-        return "Student07{" +
+        return "Student08{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", grade=" + grade +
-                ", bookList=" + bookList +
+                ", booklist=" + booklist +
                 '}';
     }
 }
