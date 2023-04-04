307c1ce224e913e73f4a25e9a92d9ec2f0f8f268
Ahmet-SAAT <avahmetsaat@gmail.com>
2023-04-05 00:01:26 +0300

    05.04.2023

---
+++ src/main/java/com/hb14/entity_life_cycle/RunnerSave14.java
@@ -0,0 +1,44 @@
+package com.hb14.entity_life_cycle;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave14 {
+
+    public static void main(String[] args) {
+
+        Student14 student1 = new Student14(); // !!! Transient
+        student1.setName("Kasim");
+        student1.setMathGrade(55);
+
+        Student14 student2 = new Student14(); // !!! Transient
+        student2.setName("Gulsen");
+        student2.setMathGrade(65);
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student14.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(student1); // !!! persistent state
+
+        student1.setName("hakki");
+
+        session.evict(student1); // detached state
+
+        student1.setName("hakki"); // bu asamada obje lif-cycle olarak hala detach asamasinda oldugu icin isim degisikligi gerceklesmez
+
+        session.update(student1); // persistent scope// Detached olmuş student1 objesini Persisted State'e alır
+        session.merge(student1); // persistent scope// Detached olmuş student1 objesini Persisted State'e alır
+
+// Bir obje new'lendiği zaman Hibernate o objeyi hemen takip etmez, DB ile bağlantısının kurulmasını bekler.
+// save gibi methodlar ile DB ile direkt fiziksel bağlantıya geçtiği an Hibernate bu objeyi takip etmeye başlar
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
